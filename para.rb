b3d8d36c962ee288209cdf4f9e0b3f8661c28933
Brandon Jabr <brandonjabr@MacJabr-Pro.local>
2018-06-06 18:08:51 -0700

    v1.3 released

--- para.rb
+++ para.rb
@@ -3,7 +3,7 @@ HOMEBREW_PARA_VERSION='1.0.0'
 class Para < Formula
   desc "The fastest file downloader for command line"
   homepage "https://github.com/brandonjabr/para"
-  url "https://github.com/brandonjabr/para/archive/v1.0b.tar.gz"
+  url "https://github.com/brandonjabr/para/archive/v1.3.tar.gz"
   depends_on "lftp" => :build
 
 def install
