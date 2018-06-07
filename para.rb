8040ae7995dcdf333b6f99ffea57606c5cf30671
Brandon Jabr <brandonjabr@MacJabr-Pro.local>
2018-06-06 18:06:49 -0700

    first brew release

--- para.rb
+++ para.rb
@@ -3,12 +3,12 @@ HOMEBREW_PARA_VERSION='1.0.0'
 class Para < Formula
   desc "The fastest file downloader for command line"
   homepage "https://github.com/brandonjabr/para"
-  head "https://github.com/brandonjabr/para.git"
+  url "https://github.com/brandonjabr/para/archive/v1.0b.tar.gz"
   depends_on "lftp" => :build
 
 def install
     system "chmod +x ./para"
-    bin.install 'para'
-    system "chmod +x /usr/local/bin/para"
+    bin.install("para")
+    system "chmod +x $(which para)"
   end
 end
